4ef387ecd75d4c7b8f8963c67425d0ed9565814f
Man Luu <man.luu@mavs.uta.edu>
2021-01-22 03:00:24 -0600

    message

---
+++ P01/bonus/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    
+    string name;
+    cout << "What's your name? ";
+    cin >> name;
+    //getline (cin, name);
+    cout << "Hello, " << name << endl;
+    
+    return 0;
+}
